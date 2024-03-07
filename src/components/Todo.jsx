a6c480590f600b706fd234cad8b6ae03375245ad
annuragsingh2017 <annuragsingh2017@gmail.com>
2024-03-07 19:42:58 +0530

    Add search functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodos, deleteTodos, updateTodo } from "../Action/actions";
 
 const Todo = () => {
   const [task, setTask] = useState("");
   const [isEdit, setIsEdit] = useState(false);
-  const [editedData, setEditedData] = useState();
+  const [editedData, setEditedData] = useState("");
+  const [searchData, setSearchData] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
   const dispatch = useDispatch();
   const todos = useSelector((data) => data.todos);
 
@@ -16,6 +18,7 @@ const Todo = () => {
     ];
 
     dispatch(addTodos(allTodosData));
+    setTask("");
   };
   const handleDelete = (id) => {
     dispatch(deleteTodos(id));
@@ -29,6 +32,15 @@ const Todo = () => {
     dispatch(updateTodo(editedData?.id, task));
     setIsEdit(false);
   };
+  useEffect(() => {
+    const { todo: todoToBeFiltered } = todos; // Destructure todo from state
+    setFilteredData(
+      todoToBeFiltered.filter((data) => data.text.includes(searchData))
+    );
+  }, [searchData, todos]);
+  const searchHandler = (e) => {
+    setSearchData(e.target.value);
+  };
   return (
     <>
       <h1>TODO</h1>
@@ -38,10 +50,15 @@ const Todo = () => {
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
+        <input
+          placeholder="Search..."
+          value={searchData}
+          onChange={searchHandler}
+        />
         <button onClick={isEdit ? handleUpdate : handleSubmit}>
           {isEdit ? "update" : "Add"}
         </button>
-        {todos?.todo?.map((data) => (
+        {filteredData?.map((data) => (
           <div key={data.id}>
             <li>{data.text}</li>
             <button onClick={() => handleDelete(data.id)}>delete</button>
